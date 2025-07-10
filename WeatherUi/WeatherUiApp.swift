1631808e87f7273aaea06dc626ee8c5b4083a1b2
Misson Paudel <missonpaudel103@gmail.com>
2025-07-10 11:20:36 +0545

    Initial Commit

---
+++ WeatherUi/WeatherUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUiApp.swift
+//  WeatherUi
+//
+//  Created by Misson Paudel on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherUiTests/WeatherUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUiTests.swift
+//  WeatherUiTests
+//
+//  Created by Misson Paudel on 10/07/2025.
+//
+
+import Testing
+@testable import WeatherUi
+
+struct WeatherUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
